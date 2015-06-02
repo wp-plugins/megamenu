b57a5285fbf74da8bb26de6312a2304ad291d017
megamenu <megamenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 12:55:11 +0000

    Restore mega menu settings on nav menus page
    git-svn-id: https://plugins.svn.wordpress.org/megamenu/trunk@1172769 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/nav-menus.class.php
+++ classes/nav-menus.class.php
@@ -42,7 +42,7 @@ class Mega_Menu_Nav_Menus {
 
         add_action( 'admin_init', array( $this, 'register_nav_meta_box' ), 9 );
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_menu_page_scripts' ) );
-
+        add_action( 'megamenu_save_settings', array($this, 'save') );
         add_filter( 'hidden_meta_boxes', array( $this, 'show_mega_menu_metabox' ) );
 
         if ( function_exists( 'siteorigin_panels_admin_enqueue_scripts' ) ) {
@@ -69,7 +69,7 @@ class Mega_Menu_Nav_Menus {
                 if ( $value == 'mega_menu_meta_box' ) {
                     unset( $hidden[$key] );
                 }
-            }            
+            }
         }
 
         return $hidden;
@@ -88,7 +88,7 @@ class Mega_Menu_Nav_Menus {
 
             add_meta_box(
                 'mega_menu_meta_box',
-                __("Max Mega Menu Status", "megamenu"),
+                __("Mega Menu Settings", "megamenu"),
                 array( $this, 'metabox_contents' ),
                 'nav-menus',
                 'side',
@@ -109,7 +109,7 @@ class Mega_Menu_Nav_Menus {
 
         if( 'nav-menus.php' != $hook )
             return;
-        
+
         // http://wordpress.org/plugins/image-widget/
         if ( class_exists( 'Tribe_Image_Widget' ) ) {
             $image_widget = new Tribe_Image_Widget;
@@ -122,7 +122,8 @@ class Mega_Menu_Nav_Menus {
         wp_enqueue_script( 'mega-menu', MEGAMENU_BASE_URL . 'js/admin.js', array(
             'jquery',
             'jquery-ui-core',
-            'jquery-ui-sortable'),
+            'jquery-ui-sortable',
+            'jquery-ui-accordion'),
         MEGAMENU_VERSION );
 
         wp_enqueue_script( 'colorbox', MEGAMENU_BASE_URL . 'js/colorbox/jquery.colorbox-min.js', array( 'jquery' ), MEGAMENU_VERSION );
@@ -142,13 +143,60 @@ class Mega_Menu_Nav_Menus {
     }
 
     /**
-     * Show the Mega Menu settings
+     * Show the Meta Menu settings
      *
      * @since 1.0
      */
     public function metabox_contents() {
 
         $menu_id = $this->get_selected_menu_id();
+
+        do_action("megamenu_save_settings");
+
+        $this->print_enable_megamenu_options( $menu_id );
+
+    }
+
+
+    /**
+     * Save the mega menu settings (submitted from Menus Page Meta Box)
+     *
+     * @since 1.0
+     */
+    public function save() {
+
+        if ( isset( $_POST['menu'] ) && $_POST['menu'] > 0 && is_nav_menu( $_POST['menu'] ) && isset( $_POST['megamenu_meta'] ) ) {
+
+            $submitted_settings = $_POST['megamenu_meta'];
+
+            if ( ! get_option( 'megamenu_settings' ) ) {
+
+                add_option( 'megamenu_settings', $submitted_settings );
+
+            } else {
+
+                $existing_settings = get_option( 'megamenu_settings' );
+
+                $new_settings = array_merge( $existing_settings, $submitted_settings );
+
+                update_option( 'megamenu_settings', $new_settings );
+
+            }
+
+            do_action( "megamenu_after_save_settings" );
+
+        }
+
+    }
+
+
+    /**
+     * Print the custom Meta Box settings
+     *
+     * @param int $menu_id
+     * @since 1.0
+     */
+    public function print_enable_megamenu_options( $menu_id ) {
 
         $tagged_menu_locations = $this->get_tagged_theme_locations_for_menu_id( $menu_id );
         $theme_locations = get_registered_nav_menus();
@@ -161,60 +209,127 @@ class Mega_Menu_Nav_Menus {
 
         } else if ( ! count ( $tagged_menu_locations ) ) {
 
-            echo "<p>" . __("This menu is not assigned to a Theme Location.", "megamenu") . "</p>";
+            echo "<p>" . __("Please assign this menu to a theme location to enable the Mega Menu settings.", "megamenu") . "</p>";
 
         } else { ?>
 
-            <table>
-                <tr>
-                    <th><?php _e("Theme Location", "megamenu"); ?></th>
-                    <th class='status'><?php _e("Enabled", "megamenu"); ?></th>
-                </tr>
+            <?php if ( count( $tagged_menu_locations ) == 1 ) : ?>
 
-                <?php foreach ( $theme_locations as $location => $name ) : ?>
-                
-                    <?php if ( isset( $tagged_menu_locations[ $location ] ) ): ?>
+                <?php
 
-                        <tr>
-                            <td><?php echo esc_html( $name ); ?></td>
-                            <td class='status'>
-                                <?php 
+                $locations = array_keys( $tagged_menu_locations );
+                $location = $locations[0];
 
-                                if (isset( $saved_settings[$location]['enabled'] )) {
-                                    ?><i class='dashicons dashicons-yes'></i><?php
-                                } else {
-                                    ?><i class='dashicons dashicons-no'></i><?php
-                                }
+                if (isset( $tagged_menu_locations[ $location ] ) ) {
+                    $this->settings_table( $location, $saved_settings );
+                }
 
-                                ?>
-                            </td>
-                        </tr>
-                        
-                    <?php endif; ?>
-                
-                <?php endforeach;?>
+                ?>
 
-            </table>
+            <?php else: ?>
+
+                <div id='megamenu_accordion'>
+
+                    <?php foreach ( $theme_locations as $location => $name ) : ?>
+
+                        <?php if ( isset( $tagged_menu_locations[ $location ] ) ): ?>
+
+                            <h3 class='theme_settings'><?php echo esc_html( $name ); ?></h3>
+
+                            <div class='accordion_content' style='display: none;'>
+                                <?php $this->settings_table( $location, $saved_settings ); ?>
+                            </div>
+
+                        <?php endif; ?>
+
+                    <?php endforeach;?>
+                </div>
+
+            <?php endif; ?>
 
             <?php
-                $url = esc_url( add_query_arg(
-                    array(
-                        'page'=>'megamenu_settings',
-                        'tab'=>'menu_settings'
-                    ),
-                    admin_url("themes.php")
-                ) );
 
-            ?>
-            <a href='<?php echo $url ?>' class='button button-secondary'><?php _e("Menu Settings", "megamenu"); ?></a>
-
-
-            <?php 
+            submit_button( __( 'Save' ), 'button-primary alignright');
 
         }
 
     }
 
+    /**
+     * Print the list of Mega Menu settings
+     *
+     * @since 1.0
+     */
+    public function settings_table( $location, $settings ) {
+        ?>
+        <table>
+            <tr>
+                <td><?php _e("Enable", "megamenu") ?></td>
+                <td>
+                    <input type='checkbox' name='megamenu_meta[<?php echo $location ?>][enabled]' value='1' <?php checked( isset( $settings[$location]['enabled'] ) ); ?> />
+                </td>
+            </tr>
+            <tr>
+                <td><?php _e("Event", "megamenu") ?></td>
+                <td>
+                    <select name='megamenu_meta[<?php echo $location ?>][event]'>
+                        <option value='hover' <?php selected( isset( $settings[$location]['event'] ) && $settings[$location]['event'] == 'hover'); ?>><?php _e("Hover", "megamenu"); ?></option>
+                        <option value='click' <?php selected( isset( $settings[$location]['event'] ) && $settings[$location]['event'] == 'click'); ?>><?php _e("Click", "megamenu"); ?></option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><?php _e("Effect", "megamenu") ?></td>
+                <td>
+                    <select name='megamenu_meta[<?php echo $location ?>][effect]'>
+                    <?php
+
+                        $selected = isset( $settings[$location]['effect'] ) ? $settings[$location]['effect'] : 'disabled';
+
+                        $options = apply_filters("megamenu_effects", array(
+                            "disabled" => array(
+                                'label' => __("None", "megamenu"),
+                                'selected' => $selected == 'disabled',
+                            ),
+                            "fade" => array(
+                                'label' => __("Fade", "megamenu"),
+                                'selected' => $selected == 'fade',
+                            ),
+                            "slide" => array(
+                                'label' => __("Slide", "megamenu"),
+                                'selected' => $selected == 'slide',
+                            )
+                        ), $selected );
+
+                        foreach ( $options as $key => $value ) {
+                            ?><option value='<?php echo $key ?>' <?php selected( $value['selected'] ); ?>><?php echo $value['label'] ?></option><?php
+                        }
+
+                    ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><?php _e("Theme", "megamenu"); ?></td>
+                <td>
+
+                    <select name='megamenu_meta[<?php echo $location ?>][theme]'>
+                        <?php
+                            $style_manager = new Mega_Menu_Style_Manager();
+                            $themes = $style_manager->get_themes();
+
+                            foreach ( $themes as $key => $theme ) {
+                                echo "<option value='{$key}' " . selected( $settings[$location]['theme'], $key ) . ">{$theme['title']}</option>";
+                            }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+
+            <?php do_action( 'megamenu_settings_table', $location, $settings ); ?>
+        </table>
+        <?php
+    }
 
 
     /**
@@ -285,7 +400,6 @@ class Mega_Menu_Nav_Menus {
         return $nav_menu_selected_id;
 
     }
-
 }
 
 endif;
